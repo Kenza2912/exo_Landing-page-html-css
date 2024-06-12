31fb6dbc957a077b7218da67c5bec160166a470c
Kenza <a.kenza29@gmail.com>
2024-06-12 17:30:12 +0200

    suite exo

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const menuBurger = document.querySelector(".menu-burger")
+console.log(menuBurger)
+
+const nav = document.querySelector(".nav")
+console.log(nav)
+
+menuBurger.addEventListener("click", ()=> {
+    menuBurger.classList.toggle("active")
+    nav.classList.toggle("active")
+})
